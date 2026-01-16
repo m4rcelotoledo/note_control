066105b24b79d657a2225e78b2dea70541005282
Marcelo Toledo <marcelotoledo5000@gmail.com>
2026-01-15 22:20:58 -0300

    Clean up

--- app/controllers/welcome_controller.rb
+++
@@ -1,9 +0,0 @@
-class WelcomeController < ApplicationController
-  def index
-    # Render Inertia page with props
-    render inertia: "Welcome", props: {
-      message: "Welcome to your Rails + React + TypeScript + Tailwind app!",
-      timestamp: Time.current.iso8601
-    }
-  end
-end
